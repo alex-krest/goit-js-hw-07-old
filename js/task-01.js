38d02606e9404f91b061b1a83472934270c3de50
alex-krest <73220512+alex-krest@users.noreply.github.com>
2021-04-22 00:34:10 +0300

    Сделано 1,2

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,15 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
 // Например для первой категории получится:
-
 // Количество элементов: 4
+const amountCategories = document.querySelectorAll(".item");
+console.log(amountCategories);
+
+console.log(`В списке ${amountCategories.length} категории`);
+
+amountCategories.forEach((el) =>
+  console.log(
+    `Текст заголовка: ${el.firstElementChild.textContent}`,
+    `Количество елементов ${el.lastElementChild.children.length}`
+  )
+);
--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,23 @@
 // В JS есть массив строк.
-
-// const ingredients = [
-//   'Картошка',
-//   'Грибы',
-//   'Чеснок',
-//   'Помидоры',
-//   'Зелень',
-//   'Приправы',
-// ];
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
+// const ing = ingredients.forEach((el) =>
+//   console.log(document.createElement("li"))
+// );
+
+const element = ingredients.map((ing) => {
+  const item = document.createElement("li");
+  item.textContent = [ing];
+  return item;
+});
+console.log(...element);
+
+const list = document.querySelector("#ingredients");
+list.append(...element);
