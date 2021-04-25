9cdbbe035127b26f64f9953061555da97ed04a82
alex-krest <73220512+alex-krest@users.noreply.github.com>
2021-04-25 22:15:58 +0300

    Без 8-й задачи

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,9 @@
 // Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
 const sizeControl = document.querySelector("#font-size-control");
 const sizeText = document.querySelector("#text");
-console.log(sizeText.textContent.style.fontSize);
 
 sizeControl.addEventListener("input", changeSize);
-function changeSize(e) {
-  //   console.log(e.currentTarget.value);
-  sizeText.textContent.style.fontSize = e.currentTarget.value;
+
+function changeSize() {
+  sizeText.setAttribute("style", `font-size: ${sizeControl.value}px`);
 }
--- js/task-08.js
+++ js/task-08.js
@@ -5,3 +5,37 @@
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+const controls = document.querySelector("#controls");
+// console.log(controls);
+
+const input = controls.firstElementChild;
+console.log(input);
+
+const btnRender = document.querySelector("button[data-action='render']");
+console.log(btnRender);
+
+const btnDestroy = document.querySelector("button[data-action='destroy']");
+console.log(btnDestroy);
+
+const boxes = document.querySelector("#boxes");
+console.log(boxes);
+
+const amount = input.value;
+console.log(amount);
+
+btnRender.addEventListener('click', createBoxes);
+function createBoxes(amount) {
+	const divOne = document.createElement('div');
+	divOne.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
+	const divWidth = divOne.style.width : 30 px;
+	const divHeigth = divOne.style.width : 30 px;
+	const divCollection = divOne * amount;
+	
+	boxes.insertAdjacentElement( "afterbegin", divCollection);
+};
+
+btnDestroy.addEventListener('click', clean);
+function clean() {
+	input.textContent = '';
+	boxes.innerHTML = '';
+};
